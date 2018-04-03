54ee4258a6a2aa2bc7d73280353c6d7ff8e0f4a4
emorgan05 <emorgan05@gmail.com>
2018-04-03 15:14:53 -0400

    Adding a list of heroes to display

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: "Star Lord" },
+  { id: 12, name: "Ironman" },
+  { id: 13, name: "Wonder Woman" },
+  { id: 14, name: "Black Widow" },
+  { id: 15, name: "Rocket" },
+  { id: 16, name: "Spiderman" },
+  { id: 17, name: "Thor" },
+  { id: 18, name: "Dr. Strange" },
+  { id: 19, name: "Hulk" },
+  { id: 20, name: "Captain America" }
+];
